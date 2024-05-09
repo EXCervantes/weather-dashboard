0058ca770229fd53582d4fc6030654cb56a03ff3
EXCervantes <160692371+EXCervantes@users.noreply.github.com>
2024-05-09 17:52:31 -0500

    Edited HTML, CSS, README, JS, and added a screenshot

--- README.md
+++ README.md
@@ -2,24 +2,17 @@
 
 ## Description
 
-This project involved creating a task board using several APIs and Libraries. The data collected would dynamically create the necessary elements on the page and populate with the data. Bootstrap was used in the HTML to implement a modal for adding a task inside the box and then a form was created within the modal box to take in the data a user's inputs.  
+This is a dashboard used to display a user's inputted city's current weather and its 5 day forecast via the [OpenWeatherMap APIs](https://openweathermap.org/). This project taught me a couple of new things such as a function I could use to extract data from an object using `Object.values`, as noted in credits below, and more practice working with promises and creating HTML elements with Javascript.
 
-The first step into populating the table was to give each task a unique ID. There were a couple ways I tried such as using `Math.floor(Math.random())` in combination with `.splice()`. But ultimately I decided it was better to use a secure non-sequential method to generate the ID after reading documentation noting methods relying on `Math.random()` do not guarantee completely unique results. So by using `crypto.randomUUID()` which I had spotted in the Mini Project earlier in this week's classwork, the random unique ID was created for the tasks. The article where I found more information on this method is noted below.
+As before with projects I laid out a concept structure of what I wanted the working product to look like via HTML and CSS. But first to get a proper structure I implemented the weather API by calling it in a `fetch()` request then analyzing the returned values. Afterwards, I grabbed the values from the returned object and stored them variable to be used later.
 
-Next up was creating the function to create Task Cards and the corresponding elements for them. This was very familiar from last week's Module assignment and this week's Mini Project. Using what I learned from both exercises and jQuery, I wrote the needed code to create the HTML elements for each task and append them to the proper HTML ID.
+Next was to handle the user search for a city and giving an error message for a blank entry. Later I would add the calls for the weather now and 5 day forecast card creating functions. I also wrote another `fetch()` using the API to specifically get he latitude and longitude.
 
-For the next step I wanted to create the functions to add the new task and store them into the localStorage before working on rendering them. Once again this week's Mini Project and previous homework exercises helped immensely with this part.
+Then I got the values of a city's forecast from the "weatherData"'s object keys. I stored these in keys to be retrieved later. I also created the elements for the today's weather forecast card and the 5 day forecast cards and appended them both to the HTML. I also retrieved the weather data from the localStorage.
 
-I wanted to render the tasks onto the page to make sure everything was working properly so far so this was the next step I tackled. First I wanted to make sure the task swim lanes were cleared so the page can dynamically update from localStorage as needed. Then I used a `for` loop to assign the tasks to their proper location based on the user's actions. Lastly, was to make the cards draggable and wrote a function inspired by the Mini Project to accomplish this.
-
-Once I made sure things looked good so far I wanted to work on updating the localStorage when the user moves a card to a different lane. Again previous practice helped a bunch with this. I used jQuery to assign the "newStatus" to a specific card based on its ID and then update that status via the localStorage. That was upon page reload the table would stay current to what the user had in their own localStorage.
-
-Now it was time to handle when a card is deleted from the task table. Once again I used jQuery to grab the necessary elements based on their ID then if it matches the Event Listener function of getting removed it disappears from the localStorage.
-
-Lastly, was to call the "renderTaskList" function to pull the items from localStorage. Then it was time to implement the date picker for the date field using the Day.js API. Then using jQuery implement the draggable cards to be dropped into the lanes.
-
-
-[Link to deployed application.](https://excervantes.github.io/task-board/)
+The next few functions were to created clear the daily weather and 5 day forecast, create the button for each respective searched city, and to repopulate the cards when a user clicks one of the past search buttons.
+ 
+[Link to deployed application.](https://excervantes.github.io/weather-dashboard/)
 
 ## Installation
 
@@ -27,31 +20,28 @@ N/A
 
 ## Usage
 
-To add a new task click the "Add Task" button to open up a modal box. There you you can enter a task title, select a due date from the date picker, and a task description. Each task will color code based on if the task has a due date for the next day and beyond, day of, and past due with white, yellow, and red representing each status respectively.
+Upon page load you will be presented with a page with a box to enter your desired city. Once you search for a city it will return the current weather in the top right box displaying city name, current conditions as represented by the weather icon, temperature, wind, and humidity.
 
-Each new task will automatically appear in the "To Do" column. You can drag each task into either the "In Progress" or "Done" columns. Once a task is complete or not needed simply click "Delete" within that task's card to remove it completely from the task board.
+Below that you will see a 5 day forecast of the same city displaying the date, current conditions as represented by the weather icon, temperature, wind, and humidity.
 
-The task will all dynamically update whether they are moved to another lane or if they are deleted. Reloading the page will retain the last state of the tasks on the board.
+You can search more cities and as you do a button will appear below that search box displaying each previous search. You can click those buttons to once again display that respective city's weather info.
 
 
 ### Example of Webpage
 
-![Website Screenshot](assets/images/taskboardscreenshot.jpg)
+![Website Screenshot](assets/images/weatherdashboardscreenshot.jpg)
 
 ## Library Elements Used
 
-[Modal](https://getbootstrap.com/docs/5.0/components/modal/)
+[Bootstrap](https://getbootstrap.com/docs/5.0/getting-started/introduction/)
 
-[Form](https://getbootstrap.com/docs/5.0/forms/overview/)
+## APIs Used
 
-[Day.js](https://day.js.org/en/)
-
-[jQuery](https://jquery.com/)
-
+[OpenWeatherMap](https://openweathermap.org/)
 
 ## Credits 
 
-"_javascript - How do I create a GUID / UUID? - Stack Overflow_. (2024, March 23). Stack Overflow. Retrieved April 25, 2024, from https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid/2117523
+"_javascript - Object.values vs Array.from, what's the difference? - Stack Overflow_. (2022, March 3). Stack Overflow. Retrieved May 7, 2024, from https://stackoverflow.com/questions/71336776/object-values-vs-array-from-whats-the-difference
 
 ## License
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,45 @@
-const weatherAPIKey = "882499a76b7f0c68ee718535237dda81";
+const weatherAPIKey = "8c07d64be9b2561b4616c459acb47fcd";
 const weatherNow = document.getElementById("weather-now");
 const weatherContainer = document.getElementById("weather-cards");
 const cityWeatherCards = document.getElementById("city-name-card");
 const searchForm = document.getElementById("search-form")
+const searchHistory = document.getElementById("history")
 const units = "imperial";
 let temperatureSymbol = "ºF";
 
+// Store user search, handle the errors, and clear stored result and reload upon page load
 const handleWeatherSearchSubmit = (event) => {
     event.preventDefault();
 
     const userCitySearch = document.getElementById("search-input").value.trim();
 
     console.log(userCitySearch);
-
+    document.getElementById("search-input").value
     if (!userCitySearch) {
         alert("Please enter a city");
         return;
     }
 
-    localStorage.setItem("user-city", JSON.stringify(userCitySearch));
     getForecast(userCitySearch)
         .then((weatherValues) => {
             console.log(weatherValues)
-            // cards
+            weatherNow.innerHTML = ""
+            weatherContainer.innerHTML = ""
             const todayCard = createTodayCard(weatherValues.today, weatherValues.cityName)
             weatherNow.appendChild(todayCard)
             for (day of weatherValues.forecast) {
                 const forecastCard = createFiveDayCard(day)
                 weatherContainer.appendChild(forecastCard)
             }
-            // save city data tolocalStorage
+
+            const storedSearches = JSON.parse(localStorage.getItem("storedSearches")) || {}
+            storedSearches[weatherValues.cityName] = weatherValues
+            localStorage.setItem("storedSearches", JSON.stringify(storedSearches))
+            loadStoredSearches();
         })
 }
 
+// Get the values for the forecast of a city
 const getForecast = (city) => {
     return cityToLatLong(city)
         .then((latLon) => {
@@ -82,78 +89,74 @@ const getForecast = (city) => {
         });
 }
 
+// Get the latitude and longitude of the searched city and handle fetch errors
 const cityToLatLong = (city) => {
     const apiURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${weatherAPIKey}`
 
-    // return fetch(apiURL)
-    //     .then(function (response) {
-    //         if (!response.ok) {
-    //             alert(response.status + " " + response.statusText);
-    //             return;
-    //         }
+    return fetch(apiURL)
+        .then(function (response) {
+            if (!response.ok) {
+                alert(response.status + " " + response.statusText);
+                return;
+            }
 
-    //         return response.json();
-    //     }).then(function (searchResult) {
+            return response.json();
+        }).then(function (searchResult) {
 
-    //         console.log(searchResult);
+            console.log(searchResult);
 
-    //         if (!searchResult.length) {
-    //             console.log('No results found!');
-    //             resultContainer.innerHTML = String.raw`<h3>No results found, search again!</h3>`;
-    //         } else {
-    //             for (const result of searchResult) {
-    //                 return {
-    //                     lat: result.lat,
-    //                     lon: result.lon,
-    //                     cityName: result.name,
-    //                 };
-    //             }
-    //         }
+            if (!searchResult.length) {
+                console.log('No results found!');
+                resultContainer.innerHTML = String.raw`<h3>No results found, search again!</h3>`;
+            } else {
+                for (const result of searchResult) {
+                    return {
+                        lat: result.lat,
+                        lon: result.lon,
+                        cityName: result.name,
+                    };
+                }
+            }
 
-    //     }).catch(function (error) {
-    //         console.error(error);
-    //         alert(error);
-    //     })
-    return Promise.resolve({
-        lat: 37.6922361,
-        lon: -97.3375448,
-        cityName: "Wichita",
-    });
+        }).catch(function (error) {
+            console.error(error);
+            alert(error);
+        })
 }
 
-// Fetch The Date Using The API
+// Fetch The Date Using The API and handle errors
 const fetchWeather = (latLon) => {
 
     const apiURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latLon.lat}&lon=${latLon.lon}&appid=${weatherAPIKey}&units=${units}`;
 
-    // console.log(apiURL);
+    console.log(apiURL);
 
-    // return fetch(apiURL)
-    //     .then(function (response) {
-    //         if (!response.ok) {
-    //             alert(response.status + " " + response.statusText);
-    //             return;
-    //         }
+    return fetch(apiURL)
+        .then(function (response) {
+            if (!response.ok) {
+                alert(response.status + " " + response.statusText);
+                return;
+            }
 
-    //         return response.json();
-    //     }).then(function (searchResult) {
+            return response.json();
+        }).then(function (searchResult) {
 
-    //         console.log(searchResult);
+            console.log(searchResult);
 
-    //         if (!searchResult.list.length) {
-    //             console.log('No results found!');
-    //             resultContainer.innerHTML = String.raw`<h3>No results found, search again!</h3>`;
-    //         } else {
-    //             return searchResult.list;
-    //         }
+            if (!searchResult.list.length) {
+                console.log('No results found!');
+                resultContainer.innerHTML = String.raw`<h3>No results found, search again!</h3>`;
+            } else {
+                return searchResult.list;
+            }
 
-    //     }).catch(function (error) {
-    //         console.error(error);
-    //         alert(error);
-    //     })
-    return Promise.resolve(forecastCache);
+        }).catch(function (error) {
+            console.error(error);
+            alert(error);
+        })
 }
-// Create Today Card
+
+// Create Today Card's elements and append them
 const createTodayCard = (todayWeather, city) => {
     const todayCard = document.createElement("div")
     todayCard.setAttribute("class", "card");
@@ -176,7 +179,7 @@ const createTodayCard = (todayWeather, city) => {
     return todayCard
 };
 
-// Create Five Day Weather Cards
+// Create 5 Forecast Day Weather Cards elements and append them
 const createFiveDayCard = (forecastWeather) => {
     const forecastDayCardContainer = document.createElement("div");
     forecastDayCardContainer.setAttribute("class", "col");
@@ -204,4 +207,42 @@ const createFiveDayCard = (forecastWeather) => {
     return forecastDayCardContainer;
 }
 
-searchForm.addEventListener('submit', handleWeatherSearchSubmit);
+// Clear search history, load stored searches, get values from the stored city weather conditions
+const loadStoredSearches = () => {
+    searchHistory.innerHTML = ""
+    const storedSearches = JSON.parse(localStorage.getItem("storedSearches")) || {}
+    for (const cityWeather of Object.values(storedSearches)) {
+        const historyButton = createSearchHistoryButton(cityWeather);
+        searchHistory.appendChild(historyButton)
+    }
+}
+
+// Create the search history button's elements and append it to the page
+const createSearchHistoryButton = (cityWeather) => {
+    const li = document.createElement("li");
+    const button = document.createElement("button")
+    button.setAttribute("class", "history")
+    button.textContent = cityWeather.cityName;
+    li.appendChild(button)
+    button.addEventListener("click", () => handleSearchHistoryClick(cityWeather))
+    return li
+}
+
+// Handle the user click to display the searched cities history onto the page 
+const handleSearchHistoryClick = (cityWeather) => {
+    console.log(cityWeather)
+    weatherNow.innerHTML = ""
+    weatherContainer.innerHTML = ""
+    cityWeather.today.date = new Date(cityWeather.today.date)
+    const todayCard = createTodayCard(cityWeather.today, cityWeather.cityName)
+    weatherNow.appendChild(todayCard)
+    for (day of cityWeather.forecast) {
+        day.date = new Date(day.date)
+        const forecastCard = createFiveDayCard(day)
+        weatherContainer.appendChild(forecastCard)
+    }
+}
+
+// User submit eventListener to load the localStorage
+searchForm.addEventListener("submit", handleWeatherSearchSubmit);
+loadStoredSearches()
